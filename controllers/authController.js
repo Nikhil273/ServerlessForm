42141cb615f48600ff7dacbacede34061a81ba76
Nikhil273 <nikhilmaurya.it@gmail.com>
2025-07-03 22:29:25 +0530

    fix: Update error response format in login and change form submission URL

--- controllers/authController.js
+++ controllers/authController.js
@@ -54,7 +54,7 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
+    return res.status(422).json({ msg: errors.array() });
   }
 
   const { email, password } = req.body;
